daabf710dc6bee254f96ac846abc7a8f327c61a2
aqsa135 <80366789+aqsa135@users.noreply.github.com>
2021-05-19 20:13:28 -0700

    Add files via upload

--- Predictor.java
+++ Predictor.java
@@ -1,9 +1,10 @@
 package project1;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public abstract class Predictor {
-	abstract ArrayList<DataPoint> readData(String filename);
+	abstract ArrayList<DataPoint> readData(String filename) throws FileNotFoundException;
 	abstract String test(DataPoint data);
 	abstract Double getAccuracy(ArrayList<DataPoint>data);
 	abstract Double getPrecision(ArrayList<DataPoint> data);
